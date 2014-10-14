a817db79a7dbec33226e018a3df978fbaf57b161
Eugene Pan <eugenep@eugenep-mbp.dhcp.thefacebook.com>
2014-10-13 20:55:48 -0700

    Initial Commit

---
+++ rottenFilms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rottenFilms
+//
+//  Created by Eugene Pan on 10/13/14.
+//  Copyright (c) 2014 Eugene Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rottenFilms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rottenFilms
+//
+//  Created by Eugene Pan on 10/13/14.
+//  Copyright (c) 2014 Eugene Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
